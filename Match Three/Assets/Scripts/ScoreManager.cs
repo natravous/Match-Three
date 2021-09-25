56614dfa70427968820caa6da20094bb10315b86
natravous <67213861+natravous@users.noreply.github.com>
2021-09-26 00:22:24 +0700

    Cek

---
+++ Match Three/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private static int highScore;
+
+    #region Singleton
+
+    private static ScoreManager _instance = null;
+
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<ScoreManager>();
+
+                if(_instance == null)
+                {
+                    Debug.LogError("Fatal Error: ScoreManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public int tileRatio;
+    public int comboRatio;
+
+    public int HighScore { get { return highScore; } }
+    public int CurrentScore { get { return currentScore; } }
+
+    private int currentScore;
+
+    void Start()
+    {
+        ResetCurrentscore();
+    }
+
+    private void ResetCurrentscore()
+    {
+        currentScore = 0;
+    }
+
+    public void IncrementCurrentScore(int tileCount, int comboCount)
+    {
+        currentScore += (tileCount * tileRatio) * (comboCount * comboRatio);
+    }
+
+    public void SetHighScore()
+    {
+        highScore = currentScore;
+    }
+}
